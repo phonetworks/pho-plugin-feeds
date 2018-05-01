690bcffef066c40e33d2be2317d96ae5c555b326
Groups <esokullu@gmail.com>
2018-05-01 10:55:59 -0700

    added missing file

---
+++ src/Pho/Plugins/Feed/Generators/JoinFeedGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pho\Plugins\Feed\Generators;
+
+use Pho\Kernel\Foundation\ParticleInterface;
+
+class JoinFeedGenerator {
+
+    public static function process(ParticleInterface $group): string
+    {
+        $feed = "";
+        return $feed;
+    }
+}
